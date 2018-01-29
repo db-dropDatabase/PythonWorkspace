4e91cb4dbfe9a66e41d7d86225bc698d02254949
Noah <noah@koontzs.com>
2018-01-28 19:26:33 -0800

    masking is hard

--- VumarkProcessing/process_data.py
+++ VumarkProcessing/process_data.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 from ShowImagesMatPlot import show_images
 from matplotlib import pyplot as plt
+from matplotlib import path
+import itertools
 
 data = ''
 
@@ -13,29 +15,47 @@ with open("first100.json", 'r') as f:
 def mapDict(item):
     str = '[' + item['picto'] + ']'
     ray = json.loads(str)
-    item['picto'] = [0, 0, 0, 0]
+    tempRay = [0, 0, 0, 0]
+    # map flat string to coordinate arrays
     if len(ray) > 0:
         for i in range(0, 4):
-            item['picto'][i] = [ray[i * 2], ray[i * 2 + 1]]
+            tempRay[i] = (ray[i * 2], ray[i * 2 + 1])
+        # sort by X coordinate
+        tempRay.sort(key=lambda coord: coord[0])
+        # if Y of point 0 is greater than Y of point 1 and the opposite is true of the Y's of the next two points, switch em
+        # also vice versa
+        if (tempRay[0][1] > tempRay[1][1] and tempRay[2][1] > tempRay[3][1]) or (tempRay[0][1] < tempRay[1][1] and tempRay[2][1] < tempRay[3][1]):
+            item['picto'] = (tempRay[0], tempRay[1], tempRay[3], tempRay[2])
+        else:
+            item['picto'] = tuple(tempRay)
+    else:
+        item['picto'] = None
     return item
 
-map(mapDict, data)
+data = list(map(mapDict, data))
 
-img = cv2.imread(data[0]['name'])
-img = cv2.pyrDown(img)
-img = cv2.pyrDown(img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+for item in data:
+    img = cv2.imread(os.path.join('C:\\Users\\Noah\\Documents\\code\\DataSet', item['type'], item['name']))
+    # check mah lines
+    if item['picto'] != None:
+        for i in range(len(item['picto']) - 1):
+            img = cv2.line(img, item['picto'][i], item['picto'][i+1], (0, 255, 0), 20)
+        # create path
+        p = path.Path(item['picto'])
+        print(tuple(itertools.product(*[range(img.shape[1]), range(img.shape[0])])))
+        mask = np.matrix(p.contains_points())
+        # make a mask using the points given
+        
 
+    img = cv2.pyrDown(img)
+    img = cv2.pyrDown(img)
 
-shape = img.shape
-print(shape)
+    shape = img.shape
 
-random = np.random.randint(0, high=255, size=shape, dtype=np.uint8)
+    #random = np.random.randint(0, high=255, size=shape, dtype=np.uint8)
 
-print("Jesus christ")
+    cv2.imshow("sigh", img)
 
-cv2.imshow("sigh", random)
+    cv2.waitKey(0)
 
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
