019cdacc5c494399da16e30fb19008d0ca8d9a9a
Noah <noah@koontzs.com>
2018-04-02 14:24:05 -0700

    whatevet this means

--- VumarkLocation/ImageProcTest.py
+++ VumarkLocation/ImageProcTest.py
@@ -6,50 +6,54 @@ from os import path
 from matplotlib import pyplot as plt
 from ShowImagesMatPlot import show_images
 
+def process(img):
+    colorSpace = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    # subtract 30 degrees from all hues values
+    val = 15
+    for row in colorSpace:
+        for pixel in row:
+            if(pixel[0] < val):
+                pixel[0] = 180 - (val - pixel[0])
+            else:
+                pixel[0] -= val
+    rs, gs, bs = cv2.split(cv2.cvtColor(colorSpace, cv2.COLOR_HSV2RGB))
+    return np.uint8((np.uint32(rs) * (255 - np.uint32(bs)) * (255 - np.uint32(gs))) >> 16)
+
+
 cwd = path.relpath(os.path.dirname(os.path.realpath(__file__)))
 #path = path.join(cwd, path.join('images', 'robotview.png'))
-path = path.join(cwd, path.join('images', '81.jpg'))
+trainPath = path.join(cwd, path.join('images', 'pictograph-center.png'))
+dataPath = path.join(cwd, path.join('images', 'pictograph-center.png'))
+
 #robotPath = path.join(cwd, 'images', 'robotview.png')
 
-img = cv2.imread(path, 1)
-img = cv2.pyrDown(img)
-img = cv2.pyrDown(img)
-img = cv2.pyrDown(img)
-img = cv2.pyrDown(img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+trainImg = cv2.imread(trainPath, 1)
+trainImg = cv2.pyrDown(trainImg)
+trainImg = cv2.cvtColor(trainImg, cv2.COLOR_BGR2RGB)
+trainImgProcessed = process(trainImg)
+dataImg = cv2.imread(dataPath, 1)
+dataImg = cv2.pyrDown(dataImg)
+dataImg = cv2.pyrDown(dataImg)
+dataImg = cv2.pyrDown(dataImg)
+dataImg = cv2.cvtColor(dataImg, cv2.COLOR_BGR2RGB)
+dataImgProcessed = process(dataImg)
 
-IMG_SHAPE = img.shape
-IMG_WIDTH = IMG_SHAPE[1]
-IMG_HEIGHT = IMG_SHAPE[0]
+orb = cv2.ORB_create()
 
-colorSpace = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+kpt, dest = orb.detectAndCompute(trainImg, None)
+kpd, desd = orb.detectAndCompute(dataImg, None)
 
-# subtract 30 degrees from all hues values
-val = 15
-for row in colorSpace:
-    for pixel in row:
-        if(pixel[0] < val):
-            pixel[0] = 180 - (val - pixel[0])
-        else:
-            pixel[0] -= val
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-colorSpace = cv2.cvtColor(colorSpace, cv2.COLOR_HSV2RGB)
-rs, bs, gs = cv2.split(colorSpace)
+matches = bf.match(desd, dest)
 
-# convert back to RGB
-r,g,b = cv2.split(img)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
 
-bWeight = np.uint8((np.uint16(r) * (255 - np.uint16(b))) >> 8)
-gWeight = np.uint8((np.uint16(r) * (255 - np.uint16(g))) >> 8)
-bothWeight = np.uint8((np.uint32(r) * (255 - np.uint32(b)) * (255 - np.uint32(g))) >> 16)
+# Draw first 10 matches.
+img3 = None
+img3 = cv2.drawMatches(dataImg, kpd, trainImg, kpt, matches[:10], img3, flags=2)
+print("here")
 
-bsWeight = np.uint8((np.uint16(rs) * (255 - np.uint16(bs))) >> 8)
-gsWeight = np.uint8((np.uint16(rs) * (255 - np.uint16(gs))) >> 8)
-bothSWeight = np.uint8((np.uint32(rs) * (255 - np.uint32(bs)) * (255 - np.uint32(gs))) >> 16)
-
-dilation = cv2.dilate(cv2.Canny(bothSWeight, 50, 250), cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)), iterations = 1)
-
-grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-show_images((img, grey, bWeight, gWeight, bothWeight, bsWeight, gsWeight, bothSWeight, dilation), cols=2, 
-    titles=("Original", "Grey", "Blue Weighted", "Green Weighted", "Both Weighted", "Shifted Blue Weighted", "Shifted Green Weighted", "Shifted Both Weighted", "Dilated"))
+plt.imshow(img3)
+plt.show()
